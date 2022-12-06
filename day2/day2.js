3e392b9e904c822f2b67da6155e16a745b1fece3
JSantilli <5582387+JSantilli@users.noreply.github.com>
2022-12-05 21:38:33 -0700

    Day 2 part 1 done

--- day2/day2.js
+++ day2/day2.js
@@ -13,12 +13,35 @@ async function processFile() {
 			crlfDelay: Infinity,
 		});
 
+		/*
+		Part 1 scoring
 
+		A rock		X	1
+		B paper		Y	2
+		C scissors	Z	3
+
+		*/
+		scores = {
+			"A X": 3 + 1,
+			"A Y": 6 + 2,
+			"A Z": 0 + 3,
+			"B X": 0 + 1,
+			"B Y": 3 + 2,
+			"B Z": 6 + 3,
+			"C X": 6 + 1,
+			"C Y": 0 + 2,
+			"C Z": 3 + 3,
+		}
+
+		let totalScore = 0;
 		rl.on('line', (line) => {
-			console.log(line);
+			totalScore += scores[line];
 		});
 
 		await events.once(rl, 'close');
+
+		// part 1, total score
+		console.log(totalScore);
 
 	} catch (err) {
 		console.log(err);
