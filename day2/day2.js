913024079ed250b04abc951e4a088d44c22a9299
JSantilli <5582387+JSantilli@users.noreply.github.com>
2022-12-05 21:48:53 -0700

    Day 2 part 2 done

--- day2/day2.js
+++ day2/day2.js
@@ -14,14 +14,22 @@ async function processFile() {
 		});
 
 		/*
-		Part 1 scoring
 
 		A rock		X	1
 		B paper		Y	2
 		C scissors	Z	3
 
 		*/
-		scores = {
+
+		/*
+
+		Part 1:
+		X = Rock
+		Y = Paper
+		Z = Scissors
+
+		*/
+		let partOneScores = {
 			"A X": 3 + 1,
 			"A Y": 6 + 2,
 			"A Z": 0 + 3,
@@ -33,15 +41,40 @@ async function processFile() {
 			"C Z": 3 + 3,
 		}
 
-		let totalScore = 0;
+		/*
+
+		Part 2:
+		X = lose
+		Y = draw
+		Z = win
+
+		*/
+		let partTwoScores = {
+			"A X": 0 + 3,
+			"A Y": 3 + 1,
+			"A Z": 6 + 2,
+			"B X": 0 + 1,
+			"B Y": 3 + 2,
+			"B Z": 6 + 3,
+			"C X": 0 + 2,
+			"C Y": 3 + 3,
+			"C Z": 6 + 1,
+		}
+
+		let partOneTotalScore = 0;
+		let partTwoTotalScore = 0;
 		rl.on('line', (line) => {
-			totalScore += scores[line];
+			partOneTotalScore += partOneScores[line];
+			partTwoTotalScore += partTwoScores[line];
 		});
 
 		await events.once(rl, 'close');
 
-		// part 1, total score
-		console.log(totalScore);
+		// part 1
+		console.log(partOneTotalScore);
+
+		// part 2
+		console.log(partTwoTotalScore);
 
 	} catch (err) {
 		console.log(err);
