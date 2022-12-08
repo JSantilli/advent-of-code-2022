0f46f7e2015c86c6a15a8da5a65cc81439f18a07
JSantilli <5582387+JSantilli@users.noreply.github.com>
2022-12-07 23:35:15 -0700

    setup of remaining days

---
+++ day17/day17.js
@@ -0,0 +1,33 @@
+
+const fs = require('fs');
+const readline = require('readline');
+const events = require('events');
+
+async function processFile() {
+
+	try {
+		const fileStream = fs.createReadStream('input.txt');
+
+		const rl = readline.createInterface({
+			input: fileStream,
+			crlfDelay: Infinity,
+		});
+
+		rl.on('line', (line) => {
+			
+		});
+
+		await events.once(rl, 'close');
+
+		// part 1
+		console.log('');
+
+		// part 2
+		console.log('');
+
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+processFile();
