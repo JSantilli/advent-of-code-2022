74cc3d11b1bd4e55937042bd501870f1c36fe172
JSantilli <5582387+JSantilli@users.noreply.github.com>
2022-12-10 01:29:09 -0700

    Done with part 2

--- day8/day8.js
+++ day8/day8.js
@@ -22,11 +22,17 @@ async function processFile() {
 		await events.once(rl, 'close');
 
 		let numberOfVisibleTrees = 0;
+		let maxScenicScore = 0;
 
 		for (let i = 0; i < grid.length; i++) {
 			for (let j = 0; j < grid[i].length; j++) {
 				if (isVisible(grid, i, j)) {
 					numberOfVisibleTrees += 1;
+				}
+
+				const currentScenicScore = getScenicScore(grid, i, j);
+				if (currentScenicScore > maxScenicScore) {
+					maxScenicScore = currentScenicScore;
 				}
 			}
 		}
@@ -35,7 +41,7 @@ async function processFile() {
 		console.log(numberOfVisibleTrees);
 
 		// part 2
-		console.log('');
+		console.log(maxScenicScore);
 
 	} catch (err) {
 		console.log(err);
@@ -106,4 +112,56 @@ function isVisible(grid, row, col) {
 	}
 
 	return false;
+}
+
+function getScenicScore(grid, row, col) {
+	
+	const maxIndex = 98;
+	const treeToCheck = grid[row][col];
+
+	if (row === 0 || row === maxIndex) {
+		return 0;
+	}
+
+	if (col === 0 || col === maxIndex) {
+		return 0;
+	}
+
+	// left
+	let leftScore = 0;
+	for (let i = col - 1; i >= 0; i--) {
+		leftScore += 1;
+		if (grid[row][i] >= treeToCheck) {
+			break
+		}
+	}
+
+	// right
+	let rightScore = 0;
+	for (let i = col + 1; i < maxIndex + 1; i++) {
+		rightScore += 1;
+		if (grid[row][i] >= treeToCheck) {
+			break;
+		}
+	}
+
+	// up
+	let upScore = 0;
+	for (let i = row - 1; i >= 0; i--) {
+		upScore += 1;
+		if (grid[i][col] >= treeToCheck) {
+			break;
+		}
+	}
+
+	// down
+	let downScore = 0;
+	for (let i = row + 1; i < maxIndex + 1; i++) {
+		downScore += 1;
+		if (grid[i][col] >= treeToCheck) {
+			break;
+		}
+	}
+
+	return leftScore * rightScore * upScore * downScore;
 }
