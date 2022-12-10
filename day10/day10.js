f6f6a6581f2d04b6a85648f363bb3973c3b680f6
JSantilli <5582387+JSantilli@users.noreply.github.com>
2022-12-10 16:38:40 -0700

    Done with part 1

--- day10/day10.js
+++ day10/day10.js
@@ -13,14 +13,34 @@ async function processFile() {
 			crlfDelay: Infinity,
 		});
 
+		let registerValue = 1;
+		let cycle = 0;
+
+		let totalSignalStrength = 0;
+
 		rl.on('line', (line) => {
+
+			const [instruction, param] = line.split(' ');
+			const instructionValue = parseInt(param);
 			
+			const cyclesToAdd = instruction === 'noop' ? 1 : 2;
+
+			for (let i = 0; i < cyclesToAdd; i++) {
+				cycle++;
+				if (cycle === 20 || cycle === 60 || cycle === 100 || cycle === 140 || cycle === 180 || cycle === 220) {
+					totalSignalStrength += cycle * registerValue;
+				}
+			}
+
+			if (instruction === 'addx') {
+				registerValue += instructionValue;
+			}
 		});
 
 		await events.once(rl, 'close');
 
 		// part 1
-		console.log('');
+		console.log(totalSignalStrength);
 
 		// part 2
 		console.log('');
