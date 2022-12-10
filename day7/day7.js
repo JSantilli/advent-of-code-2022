39a9cb7c6fc653a3c30283685775600bd014f1d9
JSantilli <5582387+JSantilli@users.noreply.github.com>
2022-12-09 23:03:13 -0700

    Created directory structure

--- day7/day7.js
+++ day7/day7.js
@@ -13,8 +13,38 @@ async function processFile() {
 			crlfDelay: Infinity,
 		});
 
+		const root = createDirectory('/', null);
+
+		let currentDirectory;
+
 		rl.on('line', (line) => {
+
+			const lineTokens = line.split(' ');
 			
+			// commands
+			if (lineTokens[0] === '$') {
+
+				if (lineTokens[1] === 'cd') {
+					if (lineTokens[2] === '..') {
+						currentDirectory = currentDirectory.parent;
+					} else {
+						if (lineTokens[2] === '/') {
+							currentDirectory = root;
+						} else {
+							currentDirectory = currentDirectory.childDirs.find(dir => dir.name === lineTokens[2]);
+						}
+					}
+				}
+
+			} else if (lineTokens[0] === 'dir') {
+
+				currentDirectory.childDirs.push(createDirectory(lineTokens[1], currentDirectory));
+			
+			} else {
+
+				currentDirectory.sizeOfFiles += parseInt(lineTokens[0]);
+
+			}
 		});
 
 		await events.once(rl, 'close');
@@ -31,3 +61,13 @@ async function processFile() {
 }
 
 processFile();
+
+function createDirectory(name, parent) {
+	
+	return {
+		name: name,
+		parent: parent,
+		sizeOfFiles: 0,
+		childDirs: [],
+	}
+}
