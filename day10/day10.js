35d0c6ab6b770280db01d5936f8b8fe8549e2c8c
JSantilli <5582387+JSantilli@users.noreply.github.com>
2022-12-10 17:32:44 -0700

    Done with part 2

--- day10/day10.js
+++ day10/day10.js
@@ -18,6 +18,8 @@ async function processFile() {
 
 		let totalSignalStrength = 0;
 
+		let screenImage = [];
+
 		rl.on('line', (line) => {
 
 			const [instruction, param] = line.split(' ');
@@ -26,9 +28,19 @@ async function processFile() {
 			const cyclesToAdd = instruction === 'noop' ? 1 : 2;
 
 			for (let i = 0; i < cyclesToAdd; i++) {
+
 				cycle++;
+
 				if (cycle === 20 || cycle === 60 || cycle === 100 || cycle === 140 || cycle === 180 || cycle === 220) {
 					totalSignalStrength += cycle * registerValue;
+				}
+
+				const cycleHorizontalPosition = (cycle - 1) % 40;
+
+				if (cycleHorizontalPosition === registerValue - 1 || cycleHorizontalPosition === registerValue || cycleHorizontalPosition === registerValue + 1) {
+					screenImage.push('#');
+				} else {
+					screenImage.push('.');
 				}
 			}
 
@@ -43,7 +55,12 @@ async function processFile() {
 		console.log(totalSignalStrength);
 
 		// part 2
-		console.log('');
+		console.log(screenImage.slice(0, 40).join(''));
+		console.log(screenImage.slice(40, 80).join(''));
+		console.log(screenImage.slice(80, 120).join(''));
+		console.log(screenImage.slice(120, 160).join(''));
+		console.log(screenImage.slice(160, 200).join(''));
+		console.log(screenImage.slice(200, 240).join(''));
 
 	} catch (err) {
 		console.log(err);
